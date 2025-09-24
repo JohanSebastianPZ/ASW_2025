03322cebce9ed420deb35dfcbc14543a94c26ebe
JohanSebastianPZ <johansebastian627@gmail.com>
2025-09-24 20:41:34 +0200

    Segundo ejercicio Uso de Variables

---
+++ laboratorio_01/ejercicio_02/script.js
@@ -0,0 +1,24 @@
+
+// 1. Declarar varialbes de tipo numer, String, booleano
+var a = "Texto"; // Variable de texto
+var b = 19;
+var c = 10;
+var d = true;
+
+// 2. Probar operaciones matematicas
+alert((b + "+" + c + "=")  + (b + c)); // El concantenador de Strings es el +
+alert((b + "-" + c + "=")  + (b - c));
+alert((b + "*" + c + "=")  + (b * c));
+alert((b + "/" + c + "=")  + (b / c));
+alert((b + "%" + c + "=")  + (b % c));
+
+// 3. Mostrar resultado por console.log
+console.log(b + "+" + c + "=", b + c);
+console.log(b, "-", c, "=", b - c);
+console.log(b, "*", c, "=", b * c);
+console.log(b, "/", c, "=", b / c);
+console.log(b, "%", c, "=", b % c);
+
+// Reto
+console.log("Texto Prueba: ", a + b)
+
