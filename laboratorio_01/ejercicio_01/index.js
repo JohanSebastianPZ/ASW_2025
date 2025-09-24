375038783e5606f0af7a223cb0727e1faba9cbbf
JohanSebastianPZ <johansebastian627@gmail.com>
2025-09-24 20:13:51 +0200

    Primer ejercicio

---
+++ laboratorio_01/ejercicio_01/index.js
@@ -0,0 +1,6 @@
+
+// Aqui uso el alert que me muestra una popUp dandome el mensaje indicado
+alert("Hola mundo");
+
+// Aqui uso el console.log que me muestra el mensaje indicado en la consola del navegador
+console.log("Hola consola");
